01034a1abcbf60c4ffcd408d697629649ccd8538
jjimenezcf <jjimenezcf@gmail.com>
2025-08-16 14:06:03 +0200

    fix: ajustes tras el rebase de reload sobre main

--- NetCore/Src/Blockchain/Blockchain.cs
+++ NetCore/Src/Blockchain/Blockchain.cs
@@ -195,16 +195,7 @@ namespace VeriFactu.Blockchain
         /// <summary>
         /// Elimina el útlimo elemento añadido a la cadena.
         /// </summary>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        /// <exception cref="InvalidOperationException">Se lanza si no se encuentra el último eslabón.</exception>
-        private void Remove() 
-=======
         private void Remove()
->>>>>>> feat: crear método reload
-=======
-        private void Remove()
->>>>>>> resolver conflictos
         {
 
             if (Previous == null && CurrentID > 1)
@@ -313,16 +304,7 @@ namespace VeriFactu.Blockchain
         /// </summary>
         /// <param name="blockchainDataFileName">Archivo de datos a restaurar.</param>
         /// <param name="blockchainDataPreviousFileName">Copia anterior utilizada para restaurar.</param>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        /// <exception cref="InvalidOperationException">Se lanza si no se encuentra archivo previo a restaurar.</exception>
-        private void RestorePreviousData(string blockchainDataFileName, 
-=======
         private void RestorePreviousData(string blockchainDataFileName,
->>>>>>> feat: crear método reload
-=======
-        private void RestorePreviousData(string blockchainDataFileName,
->>>>>>> resolver conflictos
             string blockchainDataPreviousFileName)
         {
 
@@ -523,16 +505,7 @@ namespace VeriFactu.Blockchain
         /// Añade un elemento a la cadena de bloques.
         /// </summary>
         /// <param name="registro">Registro a añadir.</param>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        /// <exception cref="Exception">Si no se puede añadir el eslabón en la cadena.</exception>
-        public void Add(Registro registro)
-=======
         public void Add(Registro registro, StringBuilder traza)
->>>>>>> feat: crear método reload
-=======
-        public void Add(Registro registro, StringBuilder traza)
->>>>>>> resolver conflictos
         {
 
             Exception addException = null;
@@ -602,18 +575,7 @@ namespace VeriFactu.Blockchain
         /// </summary>
         /// <param name="registro">Registro a eliminar.
         /// Sólo puede eliminarse el último elemento añadido.</param> 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        /// <exception cref="InvalidOperationException">
-        /// Si se itenta eliminar un registro que no es el último.
-        /// </exception>
-        public void Delete(Registro registro) 
-=======
         public void Delete(Registro registro)
->>>>>>> feat: crear método reload
-=======
-        public void Delete(Registro registro)
->>>>>>> resolver conflictos
         {
 
             if (registro.Huella != Current.Huella)
