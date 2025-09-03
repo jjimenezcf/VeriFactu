b6193cbb7b2cc6f1a6fa284428d8473c9ea1ea55
jjimenezcf <jjimenezcf@gmail.com>
2025-09-03 20:57:19 +0200

    fix: ajustes en el proceso verifactu

---
+++ NetCore/Src/Business/Operations/InvoiceQueryExtension.cs
@@ -0,0 +1,16 @@
+﻿using VeriFactu.Xml.Factu.Consulta.Respuesta;
+
+namespace VeriFactu.Net.Core.Src.Business.Operations
+{
+    public static class InvoiceQueryExtension
+    {
+        public static string GetHuellaFromQueryAeatResponse(this RespuestaConsultaFactuSistemaFacturacion queryAeatResponse, string invoiceNumber)
+        {
+            if (queryAeatResponse.RegistroRespuestaConsultaFactuSistemaFacturacion != null)
+                foreach (var registro in queryAeatResponse.RegistroRespuestaConsultaFactuSistemaFacturacion)
+                    if (registro.IDFactura.NumSerieFactura == invoiceNumber)
+                        return registro.DatosRegistroFacturacion.Huella;
+            return null;
+        }
+    }
+}
